628193bd2c8fc3514f9d0c9ced00d0ac8eddef39
David Gageot <david@gageot.net>
2016-01-27 16:19:02 +0100

    Fix default listen addr
    Signed-off-by: David Gageot <david@gageot.net>

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	url := flag.String("url", "tcp:192.168.99.100:2376", "Url of the Docker Machine")
 	certPath := flag.String("certPath", "/Users/dgageot/.docker/machine/machines/default", "Location of the certificates")
-	addr := flag.String("addr", "127.0.0.1:2376", "Address of the proxy")
+	addr := flag.String("addr", "localhost:2375", "Address of the proxy")
 	help := flag.Bool("help", false, "Show help")
 
 	flag.Parse()
